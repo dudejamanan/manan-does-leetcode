4b341759875050ace9261f7ae276146761bcbd56
Manan Dudeja <manan2006dudeja@gmail.com>
2026-07-15 22:56:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (7.29%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int empty = 0;
+        int filled = numBottles;
+        int drunk = 0;
+        while(filled > 0 || empty >= numExchange){
+            if (filled>0){
+                drunk+=filled;
+                empty+=filled;
+                filled = 0;
+            }
+            filled = empty/numExchange;
+            empty = empty%numExchange;
+        }
+        return drunk;
+    }
+};
